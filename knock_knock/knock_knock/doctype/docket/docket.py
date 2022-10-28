a97919877d836c23d832baccae7b5c93024e1839
Anjana-sajeevan <anjanasajeevs@gmail.com>
2022-10-28 11:12:00 +0530

    feat: created doctype Docket

---
+++ knock_knock/knock_knock/doctype/docket/docket.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, efeone Software Lab and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class Docket(Document):
+	pass
