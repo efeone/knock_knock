8fd0304e85bb7db5787c0ff52daad5b74770a835
Anjana-sajeevan <anjanasajeevs@gmail.com>
2022-10-29 10:43:51 +0530

    feat: Created a button to change the due and added the reason to comment

--- knock_knock/knock_knock/doctype/docket/docket.js
+++ knock_knock/knock_knock/doctype/docket/docket.js
@@ -2,9 +2,51 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Docket', {
-  // refresh: function(frm){
-  //
-  // },
+  refresh: function(frm){
+    frm.set_df_property("due_date", "read_only", frm.is_new() ? 0 : 1);
+    frm.set_df_property("change_due", "hidden", frm.is_new() ? 1 : 0);
+  },
 
-  
+  change_due : function(frm){
+  let command = new frappe.ui.Dialog({
+      title: 'Enter The Reason',
+      fields: [
+          {
+              label: 'Reason',
+              fieldname: 'reason',
+              fieldtype: 'Small Text'
+          },
+      ],
+      primary_action_label: 'Submit',
+      primary_action(values) {
+          command.hide();
+          if(values.reason){
+            frappe.call({
+              method:'knock_knock.knock_knock.doctype.docket.docket.add_comment_docket',
+              args:{'reason':values.reason,
+                    'name':frm.doc.name
+            },
+              callback:function(r){
+                if (r) {
+                  frm.reload_doc()
+                 }
+              }
+            })
+          }
+      }
+  });
+
+  command.show();
+}
+
+
+
+
+
+
+
+
+
+
+
 });
