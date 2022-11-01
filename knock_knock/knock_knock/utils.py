9152bc2e9090ee4c3d202745ce5d56a8630eb564
Keerthana-ck <keerthana98323@gmail.com>
2022-11-01 15:22:50 +0530

    feat: To get notified list of Overdue dockets over WhatsApp, so that I will be aware about my overdue dockets and change the status or due date dockets.

--- knock_knock/knock_knock/utils.py
+++ knock_knock/knock_knock/utils.py
@@ -9,7 +9,6 @@ def get_access_token():
 	return frappe.utils.password.get_decrypted_password(
 	"WhatsApp Cloud API Settings", "WhatsApp Cloud API Settings", "access_token"
 	)
-
 @frappe.whitelist()
 def get_all_dockets():
 	if frappe.db.exists("Docket", {"status": "Open"}):
@@ -20,9 +19,14 @@ def get_all_dockets():
 				today = getdate(frappe.utils.today())
 				mobile_no = frappe.db.get_value('User', docket_doc.owner, 'mobile_no')
 				due_date = getdate(docket_doc.due_date)
+				now_time = now()
+				docket_due_message = 'Docket '+ docket_doc.name + ' for '+ docket_doc.subject + ' had Overdue on '+ str(docket_doc.due_date)
 				#To change status to Overdue
 				if due_date >= today:
 					change_docket_status(docket_doc)
+					if docket_doc.status == 'Overdue':
+						if mobile_no:
+							send_whatsapp_msg(mobile_no, docket_due_message)
 				if docket_doc.remind_before_unit == 'Day':
 					if docket_doc.remind_before:
 						notification_date = frappe.utils.add_to_date(due_date, days=-1*docket_doc.remind_before)
@@ -35,7 +39,20 @@ def get_all_dockets():
 						create_notification_log(docket_doc.subject, docket_doc.owner, docket_doc.description, docket_doc.doctype, docket_doc.name)
 						if mobile_no:
 							send_whatsapp_msg(mobile_no, docket_doc.description)
-
+				if docket_doc.remind_before_unit == 'Minutes':
+					if due_date:
+						time_difference = time_diff(docket_doc.due_date, now_time).total_seconds() / 60
+						reminder_before_value = docket_doc.remind_before
+						if reminder_before_value and time_difference == reminder_before_value:
+							notification_time = frappe.utils.add_to_time(due_date, time=-1*docket_doc.remind_before)
+							if notification_time == now_time:
+								create_notification_log(docket_doc.subject, docket_doc.owner, docket_doc.description, docket_doc.doctype, docket_doc.name)
+								if mobile_no:
+									send_whatsapp_msg(mobile_no, docket_doc.description)
+				else:
+					if due_date == today:
+						if mobile_no:
+							send_whatsapp_msg(mobile_no, docket_doc.description)
 @frappe.whitelist()
 def send_whatsapp_msg(mobile_no, message_body):
 	whatsapp_communication_doc = frappe.new_doc("WhatsApp Communication")
@@ -45,6 +62,7 @@ def send_whatsapp_msg(mobile_no, message_body):
 	whatsapp_communication_doc.save()
 	whatsapp_communication_doc.send_message()
 	frappe.db.commit()
+
 @frappe.whitelist()
 def create_notification_log(subject, for_user, email_content, document_type, document_name):
     notification_doc = frappe.new_doc('Notification Log')
@@ -61,7 +79,7 @@ def change_docket_status(self):
 	if self.status == 'Open':
 		current_date = getdate(today())
 		due_date = getdate(self.due_date)
-		if due_date < current_date:
+		if current_date >= due_date:
 			self.status = 'Overdue'
 			frappe.db.set_value(self.doctype, self.name, 'status', 'Overdue')
 			frappe.db.commit()
