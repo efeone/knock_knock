cd33d8e1a530511edaf0187eefdd0502f7eece14
Anjana-sajeevan <anjanasajeevs@gmail.com>
2022-11-01 09:51:53 +0530

    fix: Change due is visible when status is open and overdue

--- knock_knock/knock_knock/doctype/docket/docket.js
+++ knock_knock/knock_knock/doctype/docket/docket.js
@@ -4,49 +4,53 @@
 frappe.ui.form.on('Docket', {
   refresh: function(frm){
     frm.set_df_property("due_date", "read_only", frm.is_new() ? 0 : 1);
-    frm.set_df_property("change_due", "hidden", frm.is_new() ? 1 : 0);
-    if (frm.doc.owner != frappe.session.user){
-      frm.set_df_property("change_due", "hidden", 1)
+    if (frm.doc.owner == frappe.session.user && !frm.is_new()){
+      if (frm.doc.status == 'Open' || frm.doc.status == 'Overdue') {
+        frm.set_df_property('change_due', 'hidden', 0)
+      }
+      else {
+        frm.set_df_property('change_due', 'hidden', 1)
+      }
     }
   },
 
   change_due : function(frm){
-  let command = new frappe.ui.Dialog({
-      title: 'Enter the reason',
-      fields: [
-         {
-            label: 'New Date',
-            fieldname: 'new_date',
-            fieldtype: 'Date',
-            reqd: 1
-         },
-         {
-              label: 'Reason',
-              fieldname: 'reason',
-              fieldtype: 'Small Text'
-          },
+        let command = new frappe.ui.Dialog({
+          title: 'Enter the reason',
+          fields: [
+             {
+                label: 'New Date',
+                fieldname: 'new_date',
+                fieldtype: 'Date',
+                reqd: 1
+             },
+             {
+                  label: 'Reason',
+                  fieldname: 'reason',
+                  fieldtype: 'Small Text'
+              },
 
-      ],
-      primary_action_label: 'Submit',
-      primary_action(values) {
-          command.hide();
-          if(values){
-            frappe.call({
-              method:'knock_knock.knock_knock.doctype.docket.docket.add_docket_comment',
-              args:{
-                    'reason':values.reason,
-                    'name':frm.doc.name,
-                    'new_date': values.new_date
-                   },
-              callback:function(r){
-                if (r) {
-                  frm.reload_doc()
-                }
+          ],
+          primary_action_label: 'Submit',
+          primary_action(values) {
+              command.hide();
+              if(values){
+                frappe.call({
+                  method:'knock_knock.knock_knock.doctype.docket.docket.add_docket_comment',
+                  args:{
+                        'reason':values.reason,
+                        'name':frm.doc.name,
+                        'new_date': values.new_date
+                       },
+                  callback:function(r){
+                    if (r) {
+                      frm.reload_doc()
+                    }
+                  }
+                })
               }
-            })
           }
-      }
-  });
-  command.show();
+      });
+      command.show();
 }
 });
