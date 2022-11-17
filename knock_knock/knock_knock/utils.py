372151967eead71a8c7c6c282238c2ad25cc186e
Reshma396 <reshmakunjuty@gmail.com>
2022-11-17 15:39:52 +0530

    fix:set overdue in todo list

--- knock_knock/knock_knock/utils.py
+++ knock_knock/knock_knock/utils.py
@@ -3,6 +3,7 @@ from frappe.model.document import Document
 from frappe_meta_integration.whatsapp.doctype.whatsapp_communication.whatsapp_communication import *
 from frappe.utils import *
 import re
+from frappe.utils.user import get_users_with_role
 
 def get_access_token():
 	return frappe.utils.password.get_decrypted_password(
@@ -76,13 +77,13 @@ def daily_todo_scheduler():
 		for todo in todos:
 			todo_doc = frappe.get_doc('ToDo', todo.name)
 			today = getdate(frappe.utils.today())
-			user = todo_doc.allocated_to if todo_doc.allocated_to else todo_doc.owner
+			user = todo_doc.owner
 			mobile_no = frappe.db.get_value('User', user, 'mobile_no')
 			due_date = getdate(todo_doc.date)
 			todo_url = get_url_to_form(todo_doc.doctype, todo_doc.name)
 			todo_due_message = 'Your ToDo for *'+ remove_html_tags(todo_doc.description) + '* had Overdue on *'+ str(due_date) + '*.\nReference : ' + todo_url
-			if due_date < today:
-				change_todo_status(todo_doc)
+			if date < today:
+				get_over_due_list(self.todo)
 				create_notification_log(todo_doc.description, user, todo_due_message, todo_doc.doctype, todo_doc.name)
 				if mobile_no:
 					send_whatsapp_msg(mobile_no, todo_due_message, todo_doc.doctype, todo_doc.name)
@@ -120,12 +121,12 @@ def change_docket_status(self):
 			frappe.db.set_value(self.doctype, self.name, 'status', 'cancelled')
 			frappe.db.commit()
 #todo
-def change_todo_status(self):
-	frappe.db.set_value(self.doctype, self.name, 'status', 'Overdue')
+def change_todo_status(name):
+	frappe.db.set_value('ToDo', name, 'status', 'Overdue')
 	frappe.db.commit()
 
 def todo_after_insert(doc, method):
-	user = doc.allocated_to if doc.allocated_to else doc.owner
+	user = doc.owner
 	mobile_no = frappe.db.get_value('User', user, 'mobile_no')
 	todo_url = get_url_to_form(doc.doctype, doc.name)
 	whatsapp_msg = "New ToDo Created for you : *" + remove_html_tags(doc.description) + '*. \nReference : ' + todo_url
@@ -135,3 +136,37 @@ def todo_after_insert(doc, method):
 def remove_html_tags(text):
     clean = re.compile('<.*?>')
     return re.sub(clean, '', text)
+
+@frappe.whitelist()
+def get_over_due_list():
+	today = getdate(frappe.utils.today())
+	users = get_users_with_role('Docket User')
+	for user in users:
+		query = """
+			SELECT DISTINCT
+				name,
+				description,
+				date
+			FROM
+				`tabToDo`
+			WHERE
+				owner = '{user}' AND
+				status = 'Open' AND
+				date < '{today}'
+		""".format(user = str(user), today = today)
+		todo_doc_list = frappe.db.sql(query, as_dict = 1)
+		todo_list = []
+		todo = {}
+		if todo_doc_list:
+			for todo in todo_doc_list:
+				url = get_url_to_form('ToDo', todo.name)
+				todo_list.append(remove_html_tags(todo.description) + ' : ' + url)
+				todo_due_message = 'Your ToDo for *'+ remove_html_tags(todo.description) + '* had Overdue on *'+ str(todo.date) + '*.\nReference : ' + url
+				change_todo_status(todo.name)
+				create_notification_log(todo.description, user, todo_due_message, 'ToDo', todo.name)
+			mobile_no = frappe.db.get_value('User', user, 'mobile_no')
+			if mobile_no:
+				todo_due_message = 'Your Overdues are,'
+				for todo in todo_list:
+					todo_due_message += '\n ' + todo
+				send_whatsapp_msg(mobile_no, todo_due_message)
